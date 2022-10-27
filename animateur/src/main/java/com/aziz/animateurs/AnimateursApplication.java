0ce1f3a8f1b0494d034cbed2695e0c255a23973a
MedAziz29 <103658249+MedAziz29@users.noreply.github.com>
2022-10-27 21:38:17 +0100

    Add files via upload

---
+++ animateur/src/main/java/com/aziz/animateurs/AnimateursApplication.java
@@ -0,0 +1,22 @@
+package com.aziz.animateurs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.aziz.animateurs.entities.Animateur;
+
+@SpringBootApplication
+public class AnimateursApplication implements CommandLineRunner {
+@Autowired
+private RepositoryRestConfiguration repositoryRestConfiguration;
+public static void main(String[] args) {
+SpringApplication.run(AnimateursApplication.class, args);
+}
+@Override
+public void run(String... args) throws Exception {
+repositoryRestConfiguration.exposeIdsFor(Animateur.class);
+}
+}
